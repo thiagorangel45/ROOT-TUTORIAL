e348d14558878515df4179cb88915dfbe0288b22
Thiago Rangel <108483500+thiagorangel45@users.noreply.github.com>
2024-05-18 15:32:00 -0300

    Add files via upload

---
+++ ROOT TUTORIAL/tut12.c
@@ -0,0 +1,10 @@
+void tut12()
+{
+    TCanvas *c1 = new TCanvas();
+    TF1 *func = new TF1("func2", "exp(-[1]*x)*[0]*sin(x)", 0, 10*TMath::Pi());
+    func->SetParameter(0, 1);
+    func->SetParameter(1, 0.1);
+    func->SetTitle("");
+    func->Draw();
+    c1->Draw(); 
+}
